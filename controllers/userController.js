4c0f1dd84643178d0327197bd4f9f0ae6a74db19
buingoctai <buingoctai994@gmail.com>
2020-03-26 22:01:02 +0700

    update fuc

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,34 +1,39 @@
-var submittedUserDataHandling = require('./pythonServerContrller');
-
 exports.submitUserData = async (req, res, next) => {
+  var request = require("superagent");
   const { userName, fbUrl, techKnowledge, addKnowledge } = req.body;
-  const isStringTech = (typeof techKnowledge) === 'string';
-  const isStringAdd = (typeof addKnowledge) === 'string';
+  const isStringTech = typeof techKnowledge === "string";
+  const isStringAdd = typeof addKnowledge === "string";
+  const python_url = "http://127.0.0.1:5000";
 
-
-
-  // if
-  if (isStringTech || isStringAdd) {
-    await submittedUserDataHandling({ userName: "hihihihiiiiiiiiiii" })
-      .then(resp => {
-        console.log("resp=", resp);
-      })
-      .catch(err => {
-        console.log("err=", err);
+  let response_Tech = {};
+  let response_Add = {};
+  if (isStringTech) {
+    request
+      .post(python_url + "/userData_classification")
+      .send({ userData: techKnowledge })
+      .set("Accept", "application/json")
+      .end((err, data) => {
+        if (err) res.send(err);
+        response_Tech = JSON.parse(data.text);
       });
-
+  } else {
+    // Saving to database
   }
 
+  if (isStringAdd) {
+    request
+      .post(python_url + "/userData_classification")
+      .send({ userData: addKnowledge })
+      .set("Accept", "application/json")
+      .end((err, data) => {
+        if (err) res.send(err);
+        response_Add = JSON.parse(data.text);
+      });
+  } else {
+    // Saving to database
+  }
 
-
-
-  // // Call python server
-  // var request = require("superagent");
-  // var python_url = "http://127.0.0.1:5000";
-  // await request.post(python_url + "/userData_classification").send(req.body).set('Accept', 'application/json')
-  //   .end((err, data) => {
-  //     if (err) res.send(err);
-  //     const response = JSON.parse(data.text);
-  //     res.status(200).send({ predictiveTech: response, predictiveAdd: response });
-  //   })
+  res
+    .status(200)
+    .send({ predictiveTech: response_Add, predictiveAdd: response_Add });
 };
