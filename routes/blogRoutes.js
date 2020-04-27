2b6f594ad4d3f3411e8d76eca2be3414504bcf00
buingoctai <buingoctai994@gmail.com>
2020-04-27 23:45:03 +0700

    DEPLOY

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -3,9 +3,8 @@ const cors = require("cors");
 const router = express.Router();
 const blogController = require("./../controllers/blogController");
 
-router.options("/", cors());
-router.post("/mainPosts", cors(), blogController.getMainPosts);
-router.post("/featuredPosts", cors(), blogController.getFeaturedPosts);
-router.post("/allPost", cors(), blogController.getAllPost);
+router.post("/mainPosts", blogController.getMainPosts);
+router.post("/featuredPosts", blogController.getFeaturedPosts);
+router.post("/allPost", blogController.getAllPost);
 
 module.exports = router;
