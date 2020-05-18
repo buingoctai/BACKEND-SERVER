f7e9e02b61d189e17758ca0f9d924396773e6bec
taibn1 <taibn1@fsoft.com.vn>
2020-05-18 17:01:34 +0700

    UPDATE CIDE

--- controllers/readNewController.js
+++ controllers/readNewController.js
@@ -5,6 +5,8 @@ const {
   FIND_ALL_ARTICLES_CRAWL,
   COUNT_TOTAL_ARTICLE_CRAWL,
   ERROR_CODE,
+  INSER_ARTICLE_CRAWL
+
 } = constants;
 
 exports.getAllArticle = async (req, res) => {
@@ -24,13 +26,6 @@ exports.getAllArticle = async (req, res) => {
       const {
         recordset: [item],
       } = data;
-
-      console.log(
-        FIND_ALL_ARTICLES_CRAWL.replace("orderByValue", orderBy)
-          .replace("orderTypeValue", orderType)
-          .replace("startValue", pageSize * (pageIndex - 1))
-          .replace("pageSizeValue", pageSize)
-      );
       request.query(
         FIND_ALL_ARTICLES_CRAWL.replace("orderByValue", orderBy)
           .replace("orderTypeValue", orderType)
@@ -52,36 +47,78 @@ exports.getAllArticle = async (req, res) => {
     });
 };
 
-exports.getDetailPost = async (req, res) => {
-  const { id } = req.body;
-  const request = new sql.Request();
+exports.getAudioArticle = async (req, res) => {
+  const request = require("superagent");
+  const { text, id } = req.body;
+  const subContentList = text.split('.');
+  const contentGroup = [];
+  const audioGroup = [];
 
-  request.query(FIND_DETAIL_POST.replace("IdValue", id), (err, data) => {
-    if (err) {
-      res.statusCode = 500;
-      res.json(500);
+  let subContent = subContentList[0];
+  if (subContent.length > 2) {
+    for (let i = 1; i < subContentList.length; i++) {
+      if ((tempory = subContent + subContentList[i]).length > 200) {
+        contentGroup.push(subContent);
+        subContent = "";
+        i -= 1;
+      }
+      else {
+        subContent += subContentList[i];
+        if (i === subContentList.length - 1) {
+          contentGroup.push(subContent);
+        }
+      }
+      tempory = "";
     }
-    const {
-      recordset: [postData],
-    } = data;
-    res.json(postData);
-  });
-};
 
-exports.getAllTopic = async (req, res) => {
-  let response = [];
-  const request = new sql.Request();
+  } else {
+    contentGroup.push(text);
+  }
 
-  request.query(FIND_ALL_TOPIC, (err, data) => {
-    if (err) {
-      res.statusCode = 500;
-      res.json(500);
+  console.log(text.length);
+
+  console.log(contentGroup);
+
+  const request_new = new sql.Request();
+  request_new.query(
+    INSER_ARTICLE_CRAWL
+      .replace("ArticleIdValue", id)
+      .replace("AudioUrlValue", 'audioGroup'),
+    (err) => {
+      if (err) {
+        // res.status(500).send();
+      }
+
     }
-    const { recordset } = data;
-    recordset.map((item) => {
-      response.push(item.Topic);
-    });
+  );
 
-    res.json(response.filter((a, b) => response.indexOf(a) === b));
-  });
-};
+  for (let i = 0; i < contentGroup.length; i++) {
+    request
+      .post("https://api.fpt.ai/hmi/tts/v5")
+      .send(contentGroup[i])
+      .set('api-key', 'CBLvzIiqo1Z59C91KxEy1svZZ7vxwT9V')
+      .set('voice', 'banmai')
+      .end((err, data) => {
+        console.log('data', data.text);
+        audioGroup.push(JSON.parse(data.text).async);
+        if (i === contentGroup.length - 1) {
+          // const request = new sql.Request();
+          // request.query(
+          //   INSER_ARTICLE_CRAWL
+          //     .replace("ArticleIdValue", id)
+          //     .replace("AudioUrlValue", audioGroup.toString()),
+          //   (err) => {
+          //     if (err) {
+          //       res.status(500).send();
+          //     }
+          //     res.json({ id: id, audio: audioGroup });
+          //   }
+          // );
+          res.json({ id: id, audio: audioGroup });
+        }
+      });
+  }
+
+
+
+}
--- routes/readNewRoutes.js
+++ routes/readNewRoutes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const readNewController = require("./../controllers/readNewController");
 
 router.post("/getAllArticle", readNewController.getAllArticle);
-
+router.post("/getAudioArticle", readNewController.getAudioArticle);
 module.exports = router;
