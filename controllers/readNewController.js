ed913bb092d820ae066c527b9900ef417a66599e
buingoctai <buingoctai994@gmail.com>
2020-05-18 23:21:12 +0700

    update text to speech api

--- controllers/readNewController.js
+++ controllers/readNewController.js
@@ -5,8 +5,8 @@ const {
   FIND_ALL_ARTICLES_CRAWL,
   COUNT_TOTAL_ARTICLE_CRAWL,
   ERROR_CODE,
-  INSER_ARTICLE_CRAWL
-
+  INSER_ARTICLE_CRAWL,
+  FIND_AUDIO_ARTICLE_CRAWL,
 } = constants;
 
 exports.getAllArticle = async (req, res) => {
@@ -32,7 +32,7 @@ exports.getAllArticle = async (req, res) => {
           .replace("startValue", pageSize * (pageIndex - 1))
           .replace("pageSizeValue", pageSize),
         (err, data) => {
-          if (err) reject({ err: ERROR_CODE["500"], statusCode: 500 });
+          if (err) reject({ err: ERROR_CODE["500"], statusCode: 5 });
           const { recordset } = data;
           resolve({ data: recordset, totalRecord: item[""] });
         }
@@ -47,22 +47,23 @@ exports.getAllArticle = async (req, res) => {
     });
 };
 
-exports.getAudioArticle = async (req, res) => {
+exports.createAudioArticle = async (req, res) => {
   const request = require("superagent");
-  const { text, id } = req.body;
-  const subContentList = text.split('.');
+  const { id, text } = req.body;
+  const subContentList = text.split(".");
   const contentGroup = [];
-  const audioGroup = [];
+  const audioGroup = [
+    "https://static.openfpt.vn/text2speech-v5/short/2020-05-18/banmai.0.48157a39971f1223ae5b665f0ad12c10.mp3",
+  ];
 
   let subContent = subContentList[0];
   if (subContent.length > 2) {
     for (let i = 1; i < subContentList.length; i++) {
-      if ((tempory = subContent + subContentList[i]).length > 200) {
+      if ((tempory = subContent + subContentList[i]).length > 5000) {
         contentGroup.push(subContent);
         subContent = "";
         i -= 1;
-      }
-      else {
+      } else {
         subContent += subContentList[i];
         if (i === subContentList.length - 1) {
           contentGroup.push(subContent);
@@ -70,55 +71,56 @@ exports.getAudioArticle = async (req, res) => {
       }
       tempory = "";
     }
-
   } else {
     contentGroup.push(text);
   }
-
-  console.log(text.length);
-
-  console.log(contentGroup);
-
-  const request_new = new sql.Request();
-  request_new.query(
-    INSER_ARTICLE_CRAWL
-      .replace("ArticleIdValue", id)
-      .replace("AudioUrlValue", 'audioGroup'),
-    (err) => {
-      if (err) {
-        // res.status(500).send();
-      }
-
-    }
-  );
 
   for (let i = 0; i < contentGroup.length; i++) {
     request
       .post("https://api.fpt.ai/hmi/tts/v5")
       .send(contentGroup[i])
-      .set('api-key', 'CBLvzIiqo1Z59C91KxEy1svZZ7vxwT9V')
-      .set('voice', 'banmai')
+      .set("api-key", "CBLvzIiqo1Z59C91KxEy1svZZ7vxwT9V")
+      .set("voice", "banmai")
       .end((err, data) => {
-        console.log('data', data.text);
+        console.log("data", data.text);
         audioGroup.push(JSON.parse(data.text).async);
         if (i === contentGroup.length - 1) {
-          // const request = new sql.Request();
-          // request.query(
-          //   INSER_ARTICLE_CRAWL
-          //     .replace("ArticleIdValue", id)
-          //     .replace("AudioUrlValue", audioGroup.toString()),
-          //   (err) => {
-          //     if (err) {
-          //       res.status(500).send();
-          //     }
-          //     res.json({ id: id, audio: audioGroup });
-          //   }
-          // );
-          res.json({ id: id, audio: audioGroup });
+          const request = new sql.Request();
+          request.query(
+            INSER_ARTICLE_CRAWL.replace("ArticleIdValue", id).replace(
+              "AudioUrlValue",
+              audioGroup.toString()
+            ),
+            (err) => {
+              if (err) {
+                res.status(500).send();
+              }
+              res.json({ id: id, audio: audioGroup });
+            }
+          );
         }
       });
   }
+};
 
+exports.getAudioArticle = (req, res) => {
+  const { id } = req.body;
+  const request = new sql.Request();
 
-
-}
+  request.query(
+    FIND_AUDIO_ARTICLE_CRAWL.replace("IdValue", id),
+    (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.json(err);
+      }
+      const { recordset } = data;
+      if (recordset.length === 0) {
+        res.json({ id: id, audio: [] });
+      } else {
+        const [audioData] = recordset;
+        res.json({ id: id, audio: audioData.AudioUrl.split(",") });
+      }
+    }
+  );
+};
--- routes/readNewRoutes.js
+++ routes/readNewRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const readNewController = require("./../controllers/readNewController");
 
 router.post("/getAllArticle", readNewController.getAllArticle);
+router.post("/createAudioArticle", readNewController.createAudioArticle);
 router.post("/getAudioArticle", readNewController.getAudioArticle);
 module.exports = router;
