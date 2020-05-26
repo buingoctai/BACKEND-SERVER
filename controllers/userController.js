51ef4a5cec3b428c62af9848d13f142f61bdd6da
taibn1 <taibn1@fsoft.com.vn>
2020-05-26 16:58:19 +0700

    add notification

--- controllers/userController.js
+++ controllers/userController.js
@@ -11,6 +11,26 @@ const {
   DATABASE_SERVER_CONFIG_DEV,
 } = constants;
 
+// Push Subcription
+const subscriptions = {};
+var crypto = require('crypto');
+const webpush = require('web-push');
+
+const vapidKeys = {
+  privateKey: 'bdSiNzUhUP6piAxLH-tW88zfBlWWveIx0dAsDO66aVU',
+  publicKey:
+    'BIN2Jc5Vmkmy-S3AUrcMlpKxJpLeVRAfu9WBqUbJ70SJOCWGCGXKY-Xzyh7HDr6KbRDGYHjqZ06OcS3BjD7uAm8'
+};
+
+webpush.setVapidDetails('mailto:example@yourdomain.org', vapidKeys.publicKey, vapidKeys.privateKey);
+
+function createHash(input) {
+  const md5sum = crypto.createHash('md5');
+  md5sum.update(Buffer.from(input));
+  return md5sum.digest('hex');
+}
+
+//-------------
 exports.submitUserData = async (req, res, next) => {
   const request = require("superagent");
   const { userName, fbUrl, techKnowledge, addKnowledge } = req.body;
@@ -205,3 +225,31 @@ exports.getProfile = async (req, res) => {
 exports.example = (req, res) => {
   res.status(200).send({ author: "bui ngoc tai" });
 };
+
+
+exports.handlePushNotificationSubscription = (req, res) => {
+  const subscriptionRequest = req.body.data;
+  const susbscriptionId = createHash(JSON.stringify(subscriptionRequest));
+  subscriptions[susbscriptionId] = subscriptionRequest;
+  res.status(201).json({ id: susbscriptionId });
+}
+
+exports.sendPushNotification = (req, res) => {
+  const subscriptionId = req.params.id;
+  const pushSubscription = subscriptions[subscriptionId];
+  webpush
+    .sendNotification(
+      pushSubscription,
+      JSON.stringify({
+        title: 'New Product Available ',
+        text: 'HEY! Take a look at this brand new t-shirt!',
+        tag: 'new-product',
+        url: '/new-product-jason-leung-HM6TMmevbZQ-unsplash.html'
+      })
+    )
+    .catch((err) => {
+      console.log(err);
+    });
+
+  res.status(202).json({})
+}
